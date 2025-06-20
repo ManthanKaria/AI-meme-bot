6d26b35b7894e877ceefe576f47437fe53750ee9
Manthan Karia <91182696+ManthanKaria@users.noreply.github.com>
2025-06-20 13:36:06 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -4,13 +4,13 @@ import os
 HUGGING_FACE_API_URL = "https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-base"
 HUGGING_FACE_HEADERS = {"Authorization": f"Bearer {os.getenv('HUGGING_FACE_KEY')}"}
 
-GEMINI_API_KEY = "AIzaSyCABC7nUV8y-pgJGbM8KnviGrAVNX8zK5U"
+GEMINI_API_KEY = "Your_Gemeni_API"
 GEMINI_API_URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro-latest:generateContent?key=AIzaSyCABC7nUV8y-pgJGbM8KnviGrAVNX8zK5U"
 GEMINI_HEADERS = {
     "Content-Type": "application/json",
 }
 # 🔹 Pexel APIs (Free & Unlimited images)
-PEXELS_API_KEY = "9w2i2kPw4kJd6a93b04JAoMcC8ySgpxbSm9GUOmohn8VhspIWefWeT8t"
+PEXELS_API_KEY = "YOUR_PIXEL_API"
 PEXELS_SEARCH_URL = "https://api.pexels.com/v1/search"
 
 # 🔹 Paths
